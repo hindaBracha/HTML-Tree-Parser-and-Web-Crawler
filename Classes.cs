adae5bfbeb08491e661a409c938b19152df7394a
hinda bracha <hinda40311@gmail.com>
2024-11-19 12:20:00 +0200

    first commit

---
+++ Classes.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace projectHtml
+{
+    public class HtmlElement
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public List<string> Attributes { get; set; }
+        public List<string> Classes { get; set; }
+        public string InnerHtml { get; set; }
+        public HtmlElement Parent { get; set; }
+        public List<HtmlElement> Children { get; set; }
+        public HtmlElement()
+        {
+            Attributes = new List<string>();
+            Classes = new List<string>();
+            Children = new List<HtmlElement>();
+        }
+        //מחזירה את כל הצאצאים של הצומת שהתקבלה
+        public IEnumerable Descendants(HtmlElement root)
+        {
+            Queue<HtmlElement> queue = new Queue<HtmlElement>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                foreach (var n in queue.First().Children)
+                {
+                    if (n != null)
+                        queue.Enqueue(n);
+                }
+                yield return queue.Dequeue();
+            }
+        }
+        //מחזירה את כל האבות של הצומת שהתקבלה
+        public IEnumerable Ancestors(HtmlElement root)
+        {
+            for (HtmlElement temp = root; temp.Parent != null; temp = temp.Parent)
+            {
+                yield return temp.Parent;
+            }
+        }
+        //מקבלת סלקטור ומבצעת חיפוש בעץ מחזירה את כל הצמתים שעונים לתנאי
+        public HashSet<HtmlElement> search(HtmlElement node, selector s, HashSet<HtmlElement> listE)
+        {
+            if (s.Child == null)
+            {
+                listE.Add(node);
+                return listE;
+            }
+            if (node == null)
+                return listE;
+            var elements = Descendants(node);
+            HashSet<HtmlElement> filer = new HashSet<HtmlElement>();
+            foreach (HtmlElement element in elements)
+            {
+                bool ok1 = true, ok2 = true;
+                if ((s.id != null && s.id != element.id) || (s.TagName != null && s.TagName != element.name))
+                    ok1 = false;
+                foreach (string clas in s.Classes)
+                {
+                    if (!element.Classes.Contains(clas))
+                    {
+                        ok2 = false;
+                        break;
+                    }
+                }
+                if (ok1 && ok2)
+                    filer.Add(element);
+            }
+            foreach (HtmlElement element in filer)
+            {
+                search(element, s.Child, listE);
+
+            }
+            return listE;
+        }
+    }
+    public class HtmlHelper
+    {
+        public static HtmlHelper Instance => new HtmlHelper();
+        //private readonly static HtmlHelper aa = new HtmlHelper();
+        //public static HtmlHelper Instance => aa;
+        static string tags = File.ReadAllText("HtmlTags.json");
+        static string voidTags = File.ReadAllText("HtmlVoidTags.json");
+        public string[] arrTags { get; set; }
+        public string[] arrVoidTags { get; set; }
+        private HtmlHelper()
+        {
+            arrTags = JsonSerializer.Deserialize<string[]>(tags);
+            arrVoidTags = JsonSerializer.Deserialize<string[]>(voidTags);
+        }
+    }
+    public class selector
+    {
+        public string id { get; set; }
+        public string TagName { get; set; }
+        public List<string> Classes { get; set; }
+        public selector Parent { get; set; }
+        public selector Child { get; set; }
+        public selector()
+        {
+            this.Classes = new List<string>();
+        }
+        public static selector parsQuery(string query)
+        {
+            HtmlHelper htmlHelper = HtmlHelper.Instance;// האתחול של HtmlHelper
+            string[] arrTags = htmlHelper.arrTags; //מגיע למערך arrTags
+            selector curent = new selector();
+            selector root = new selector();
+            curent = root;
+            string[] splits = query.Split(" ");
+            foreach (string s in splits)
+            {  
+                string[] parts = Regex.Split(s, @"(?=\.|#)");
+                foreach (string part in parts)
+                {
+                    if (part!=""&&part[0] == '.')
+                        curent.Classes.Add(part[1..]);
+                    else if (part != "" && part[0] == '#')
+                        curent.id = part[1..];
+                    else if (arrTags.Contains(part))
+                        curent.TagName = part;
+                }
+                curent.Child = new selector();
+                curent = curent.Child;
+            }
+            return root;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using projectHtml;
+
+namespace consul
+{
+    public class Program
+    {
+        public static async Task<string> Load(string url)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var Html = await response.Content.ReadAsStringAsync();
+            return Html;
+        }
+        public static async Task<HtmlElement> buildTree()
+        {
+            //html-קריאת ה
+            var html = await Load("https://shaoncitaa.netlify.app/%D7%94%D7%A4%D7%A1%D7%A7%D7%94_%D7%90.html");
+            var cleanHtml = Regex.Replace(html, @"(?<!<[^>]*)\s+", "");//מסיר רווחים מתוך ה-HTML
+            // 3. מפצל את ה-HTML לתאים לפי פתיחת תגים
+            var htmlLines = new Regex("<(.*?)>").Split(cleanHtml).Where(s => s.Length > 0);
+            //יבוא מערכי העזר 
+            HtmlHelper htmlHelper = HtmlHelper.Instance;// יזהה כאן את האתחול של HtmlHelper
+            string[] arrTags = htmlHelper.arrTags; // כאן אתה מגיע למערך arrTags
+            string[] arrVoidTags = HtmlHelper.Instance.arrVoidTags;
+            //יצירת שורש העץ
+            HtmlElement root = new HtmlElement();
+            HtmlElement cElement = new HtmlElement();
+            var regex = new Regex("([^\\s]*?)=\"(.*?)\"");
+            foreach (var element in htmlLines)
+            {
+                var e = element;
+                string[] splits = e.Split(" ");
+
+                if (splits[0] == "/html")
+                    return root;
+                else if (splits[0] == "html")
+                {
+                    root.name = "html";
+                    var attributeMatches = regex.Matches(element);
+                    foreach (Match match in attributeMatches)
+                    {
+                        if (match.Groups[1].Value == "id")
+                            root.id = match.Groups[2].Value;
+                        else if (match.Groups[1].Value == "class")
+                            root.Classes.Add(match.Groups[2].Value);
+                        else root.Attributes.Add(match.Groups[1].Value);
+                        cElement = root;
+                    }
+                }
+                else if (splits[0][0] == '/')
+                {
+                    cElement = cElement.Parent;
+                }
+                else if (arrTags.Contains(splits[0]))
+                {
+                    HtmlElement newE = new HtmlElement();
+                    newE.name = splits[0];
+                    newE.Parent = cElement;
+                    cElement.Children.Add(newE);
+                    var attributeMatches = regex.Matches(element);
+                    foreach (Match match in attributeMatches)
+                    {
+                        if (match.Groups[1].Value == "id")
+                            newE.id = match.Groups[2].Value;
+                        else if (match.Groups[1].Value == "class")
+                            newE.Classes.Add(match.Groups[2].Value);
+                        else newE.Attributes.Add(match.Groups[1].Value);
+                    }
+                    if (!arrVoidTags.Contains(splits[0]))
+                        cElement = newE;
+                }
+                else if (cElement != null)
+                    cElement.InnerHtml = element.ToString();
+            }
+            return root;
+        }
+
+
+        static async Task Main(string[] args)
+        {
+
+            HtmlElement r = new HtmlElement();
+            r = await buildTree();
+            //selector selector = selector.parsQuery("body aside div.one");
+            //selector selector = selector.parsQuery("body div .flip-box");
+            selector selector = selector.parsQuery("body ");
+            HashSet<HtmlElement> a = new HashSet<HtmlElement>();
+            HtmlElement HtmlElement = new HtmlElement();
+            List<HtmlElement> s = HtmlElement.search(r, selector, a).ToList();
+            Console.WriteLine("התוצאות:");
+            foreach (HtmlElement element in s)
+            {
+                Console.WriteLine(element.name);
+            }
+            Console.WriteLine("סוף תוצאות");
+            Console.ReadLine();
+        }
+    }
+}
